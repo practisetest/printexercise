0544f545aa42661a74728ab1e69360de76642343
ravinau <Ravin.singh.au@gmail.com>
2016-02-15 19:58:15 +1100

    Updated  package structure

--- src/main/java/printexercisetest/impl/IPrintJobCostCalculator.java
+++
@@ -1,8 +0,0 @@
-package printexercisetest.impl;
-
-import java.math.BigDecimal;
-
-public interface IPrintJobCostCalculator {
-	enum JOBTYPE { BLACKANDWHITE, COLOR }
-	BigDecimal calculateCost(int noofpages,JOBTYPE type);
-}
